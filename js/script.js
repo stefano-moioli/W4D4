77e1fa2ca1e65154b5a577d5f85ee6ed83f07c3b
stefano-moioli <stefano.moioli10@gmail.com>
2024-03-31 17:23:21 +0200

    productCard

--- js/script.js
+++ js/script.js
@@ -3,18 +3,19 @@ const ENDPOINT = "https://striveschool-api.herokuapp.com/api/product/"
 const KEY = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjA4MTdjMWViYzE5NzAwMTk4ZWQxM2MiLCJpYXQiOjE3MTE4MDY0MDEsImV4cCI6MTcxMzAxNjAwMX0.barFv73SIEyQx9V2AwLRiHeMA3UPuFZNBhVJTkZTJX8"
 
 window.onload = async () => {
-        const response = await fetch(ENDPOINT, {
-        headers: { 
-        authorization: KEY,
+    const response = await fetch(ENDPOINT, {
+        headers: {
+            authorization: KEY,
         },
     })
     const products = await response.json();
     const productsContainer = document.getElementById("productsContainer");
     products.forEach((product) => {
         productsContainer.innerHTML += `
-        <div class="col-12 col-sm 6 col-md-3 col-lg-2">
+        <div class="col-12 col-sm 6 col-md-4 col-lg-3 productContainer">
         <a href="product.html?id=${product._id}"><img class="bookImg" src="${product.imageUrl}" class="card-img-top" alt="product image"></a>
         <h5 class="card-title">${product.name}</h5>
+          <p class="card-text">${product.brand} $</p>
           <p class="card-text">${product.price} $</p>
           <p class="card-text">${product.description}</p>
           <a href="backOffice.html?id=${product._id}" class="btn btn-primary">Modify Product</a>
@@ -22,7 +23,7 @@ window.onload = async () => {
           `
     })
     console.log(products);
-    } 
+}
 
 /* //Visualizzo l'array nella console
 getData().then(body => console.log(body))
